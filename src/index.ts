fd5e6a26530773363343b43bff5ce3bebfd4928d
Felipe Oliveira <flipeaz342@gmail.com>
2025-04-10 23:20:50 -0400

    feat: add socket.io for real-time chat functionality

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import express from "express";
 import { config } from "dotenv";
 import { createServer } from "node:http";
+import { join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 config();
 
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(PORT, () => {
